239e8a378fa82a849b2ab249330b4411edfe05e3
Rodrigo Paszniuk <rodrigopaszniuk@gmail.com>
2020-04-07 13:48:58 -0400

    Firebase Analytics + Crashlytics + Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:frases480/routes/routes.dart';
 import 'package:frases480/screens/categories_screen.dart';
@@ -9,9 +11,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics analytics = FirebaseAnalytics();
+
     return MaterialApp(
       theme: ThemeData.light(),
       home: HomeScreen(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       routes:  {
         Routes.home: (context) => HomeScreen(),
         Routes.categories: (context) => CategoriesScreen(),
