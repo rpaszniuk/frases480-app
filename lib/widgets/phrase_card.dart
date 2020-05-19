f95e524a9ae112e1a407647ce5b016fef6b220e1
Rodrigo Paszniuk <rodrigopaszniuk@gmail.com>
2020-05-19 10:03:20 -0400

    Improvements

--- lib/widgets/phrase_card.dart
+++ lib/widgets/phrase_card.dart
@@ -3,6 +3,7 @@ import 'package:frases480/services/phrase.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share/share.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/services.dart';
 
 class PhraseCard extends StatelessWidget {
   Phrase phrase;
@@ -11,38 +12,49 @@ class PhraseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          phrase.category == null ? ListTile(
-            title: Padding(padding: EdgeInsets.only(top: 10), child: Text(phrase.phrase))
-          )
-              : ListTile(
-            title: Padding(padding: EdgeInsets.only(top: 10), child: Text("Frase de " + phrase.category.name)),
-            subtitle: Padding(padding: EdgeInsets.only(top: 10), child: Text(phrase.phrase)),
-          ),
-          ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                child:  Icon(Icons.web),
-                onPressed: () {
-                  var url = phrase.url;
-                  if (canLaunch(url) != null) {
-                    launch(url, forceSafariVC: false);
-                  } else {
-                    throw 'No se puede abrir navegador, por favor visitar: $url';
-                  }
-                },
-              ),
-              kIsWeb ? null : FlatButton(
-                child: Icon(Icons.share),
-                onPressed: () {
-                  Share.share(phrase.phrase + ' ' + phrase.url);
-                },
-              ),
-            ],
-          ),
-        ],
+      child: new InkWell(
+        onTap: () {
+            Clipboard.setData(ClipboardData(text: phrase.phrase))
+                .catchError((e) {})
+                .whenComplete(() {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Copiado al portapapeles'),
+              ));
+            });
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            phrase.category == null ? ListTile(
+                title: Padding(padding: EdgeInsets.only(top: 10), child: Text(phrase.phrase))
+            )
+                : ListTile(
+              title: Padding(padding: EdgeInsets.only(top: 10), child: Text("Frase de " + phrase.category.name)),
+              subtitle: Padding(padding: EdgeInsets.only(top: 10), child: Text(phrase.phrase)),
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child:  Icon(Icons.web),
+                  onPressed: () {
+                    var url = phrase.url;
+                    if (canLaunch(url) != null) {
+                      launch(url, forceSafariVC: false);
+                    } else {
+                      throw 'No se puede abrir navegador, por favor visitar: $url';
+                    }
+                  },
+                ),
+                kIsWeb ? null : FlatButton(
+                  child: Icon(Icons.share),
+                  onPressed: () {
+                    Share.share(phrase.phrase + ' ' + phrase.url);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
